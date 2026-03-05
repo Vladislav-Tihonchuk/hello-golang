1177002f84ffc5ec28f3049936c5649a14e82606
Vladislav <tikhonchuk.06@mail.ru>
2026-03-05 16:20:06 +0300

    fix bag

--- main_test.go
+++ main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	want := "Hello GO"
+	want := "Hello Go"
 	got := hello()
 	if want != got {
 		t.Fatalf("want %s, got %s", want, got)
